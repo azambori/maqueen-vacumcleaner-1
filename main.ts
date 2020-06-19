c456344335e2139b89a07cbf9ae8ab02101f90eb
azambori <64330523+azambori@users.noreply.github.com>
2020-06-19 11:18:06 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,12 @@ maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
 maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
 maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
 basic.pause(500)
+let strip = neopixel.create(DigitalPin.P0, 4, NeoPixelMode.RGB)
+let range = strip.range(0, 4)
+strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
 basic.forever(function () {
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
     basic.showArrow(ArrowNames.South)
@@ -13,4 +19,8 @@ basic.forever(function () {
             basic.pause(10)
         }
     }
+})
+basic.forever(function () {
+    basic.pause(1000)
+    strip.rotate(1)
 })
