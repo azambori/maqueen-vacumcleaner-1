2519cb264e625d2c740e4fc1348ae49b1935ab37
azambori <64330523+azambori@users.noreply.github.com>
2020-06-19 11:10:29 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
+maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+basic.pause(500)
 basic.forever(function () {
-	
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
+    basic.showArrow(ArrowNames.South)
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 20) {
+        basic.showIcon(IconNames.SmallDiamond)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 10)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 10)
+        while (maqueen.Ultrasonic(PingUnit.Centimeters) < 30) {
+            basic.pause(10)
+        }
+    }
 })
