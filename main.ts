ed1c6d9a5497b8282d895918700438cb80a4509a
azambori <64330523+azambori@users.noreply.github.com>
2020-06-25 19:09:01 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    Run = 0
+})
+let Run = 0
 maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
 maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
 maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
@@ -9,15 +13,20 @@ strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
 strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
 strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
 basic.forever(function () {
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
-    basic.showArrow(ArrowNames.South)
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 20) {
-        basic.showIcon(IconNames.SmallDiamond)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 10)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 10)
-        while (maqueen.Ultrasonic(PingUnit.Centimeters) < 30) {
-            basic.pause(10)
+    if (Run == 1) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
+        basic.showArrow(ArrowNames.South)
+        while (maqueen.Ultrasonic(PingUnit.Centimeters) < 20) {
+            basic.showIcon(IconNames.SmallDiamond)
+            basic.pause(300)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 45)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 45)
+            while (maqueen.Ultrasonic(PingUnit.Centimeters) < 30) {
+                basic.pause(10)
+            }
         }
+    } else {
+        maqueen.motorStop(maqueen.Motors.All)
     }
 })
 basic.forever(function () {
